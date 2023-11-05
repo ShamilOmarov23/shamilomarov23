6dfb884385ac02f21ef2af5ae338cd6e89272167
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-05 19:53:37 +0300

    задача 1.2.4

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean varboolean=true;
+        byte varbyte=100;
+        short varshort=3266;
+        int varint=557;
+        long varlong=2023;
+        double vardouble=2023.09;
+        float varfloat=82.9F;
+        char varchar=6000;
+        {
+            System.out.println(varboolean);
+            System.out.println(varbyte);
+            System.out.println(varshort);
+            System.out.println(varint);
+            System.out.println(varlong);
+            System.out.println(vardouble);
+            System.out.println(varfloat);
+            System.out.println(varchar);
+    }
+}
+}
