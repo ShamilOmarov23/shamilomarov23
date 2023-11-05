bf1f554d8064ad57a58bce31891d9b4da03d8500
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-05 22:12:41 +0300

    Добавил 1.2.6

--- src/Task_1_2_6/MonitorsCounter.java
+++ src/Task_1_2_6/MonitorsCounter.java
@@ -1,4 +1,12 @@
-package Task_1_2_6;
 
+package Task_1_2_6;
 public class MonitorsCounter {
+    public static void main(String[] args) {
+        int result = drawisMonitorsCounter(50, 45);
+        System.out.println("result = " + result);
+    }
+    public static int drawisMonitorsCounter(int monitors, int programmers) {
+        return monitors;
+    }
 }
+
