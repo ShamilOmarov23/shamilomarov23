c52bb17da8c1f8c11fd92d058837e9d1d5046156
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-06 19:39:52 +0300

    Задача 1.2.7

---
+++ src/Task_1_2_7/AriphmeticOperations.java
@@ -0,0 +1,11 @@
+package Task_1_2_7;
+
+public class AriphmeticOperations {
+    public static void main(String[] args){
+        double result = priceCalculation (8.50, 2);
+        System.out.println("17 = " + 17);
+    }
+    public static double priceCalculation(double price, int count) {
+        return price;
+    }
+}
