45a14fa3944662f3c38e9f4b06c5e0d0a38065de
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-12 18:41:44 +0300

    Задача 1.2.10

--- src/Task_1_2_10/MathTypes.java
+++ src/Task_1_2_10/MathTypes.java
@@ -1,10 +1,15 @@
 package Task_1_2_10;
 
 public class MathTypes {
+    public static void main(String[] args) {
+        boolean d = doubleExpression(2.0, 2.0, 4.0);
+        System.out.println("d = " + d);
+    }
     public static boolean doubleExpression(double a, double b, double c) {
-        if (java.lang.Math.abs((a + b) - c) < 1E-4 )
-            return true;
-        else
-            return false;
+        boolean d = false;
+        if (Math.abs((a + b) - c) <= 1E-4) {
+            d = true;
+        }
+        return d;
     }
 }
