fc58f3cd187d4658d6758e4db3886d5b2961e3c8
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-14 21:38:38 +0300

    Задача 1.2.13

---
+++ src/Task_1_2_13/Circle.java
@@ -0,0 +1,8 @@
+package Task_1_2_13;
+
+public class Circle {
+    public static void main(String[] args) {
+         private Circle(double r);
+            double radius = r;
+            double area = Math.PI * (radius * radius); //Площадь круга вычисляется по формуле = PI*radius*radius
+    }
