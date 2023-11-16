c2671a5c4d58f6efd904cfc26f04738fb8140fd6
Шамиль Омаров <omarovsk@yandex.ru>
2023-11-16 10:22:04 +0300

    Задача 1.2.14

---
+++ src/Task_1_2_14/BigInteger.java
@@ -0,0 +1,10 @@
+package Task_1_2_14;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger result = maxLong.multiply(maxLong);
+        System.out.println(result);
+    }
+}
