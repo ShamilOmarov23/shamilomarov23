0a6a3a833f22ab9cdd545197a5a6ef9020e708fc
ShamilOmarov23 <omarovsk@yandex.ru>
2023-12-13 22:55:11 +0300

    Merge remote-tracking branch 'origin/master'

--- src/Task_1_2_14/Maximum.java
+++ src/Task_1_2_14/Maximum.java
@@ -1,14 +1,12 @@
 package Task_1_2_14;
-
 import java.math.BigInteger;
-
-public class max {
-    public static void main(String[]args){
+public class Maximum {
+    public static void main(String[] args) {
         BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
         System.out.println(maxLongSqr(maxLong));
     }
-    public static BigInteger maxLongSqr(BigInteger maxLong){
-       return maxLong.multiply(maxLong);
 
+    public static BigInteger maxLongSqr(BigInteger maxLong) {
+      return maxLong.multiply(maxLong);
     }
 }
