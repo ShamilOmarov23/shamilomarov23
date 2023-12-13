a1a5cd4242fbf9f72e6cfeac81ed69ca287f1c53
ShamilOmarov23 <omarovsk@yandex.ru>
2023-12-13 21:37:25 +0300

    1.2.14

--- src/Task_1_2_14/BigIntegerr.java
+++ src/Task_1_2_14/BigIntegerr.java
@@ -2,12 +2,13 @@ package Task_1_2_14;
 
 import java.math.BigInteger;
 
-public class main {
-    public static void main()(String[] args) {
+public class max {
+    public static void main(String[]args){
         BigInteger maxLong = BigInteger.valueOf(Long.MAX_VALUE);
         System.out.println(maxLongSqr(maxLong));
     }
     public static BigInteger maxLongSqr(BigInteger maxLong){
-       return  = maxLong.multiply(maxLong);
+       return maxLong.multiply(maxLong);
+
     }
 }
