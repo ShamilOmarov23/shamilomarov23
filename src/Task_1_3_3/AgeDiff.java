46bda4f25b8e81eaf04fb0374d512e7e551e4016
ShamilOmarov23 <omarovsk@yandex.ru>
2023-12-26 23:29:51 +0300

    Task_1_3_3

---
+++ src/Task_1_3_3/AgeDiff.java
@@ -0,0 +1,11 @@
+package Task_1_3_3;
+
+public class AgeDiff {
+    public static void main(String[] args){
+        double a1= 2.5;
+        int a2 = 3;
+        double b1 = 4.1;
+        int b2 = 3;
+
+    }
+}
