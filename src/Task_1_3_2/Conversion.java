3bc142afe77d0a6db11a2b859c8be10e6ee9ab76
ShamilOmarov23 <omarovsk@yandex.ru>
2023-12-26 13:36:42 +0300

    Merge remote-tracking branch 'origin/master'

--- src/Task_1_3_2/Conversion.java
+++ src/Task_1_3_2/Conversion.java
@@ -1,6 +1,14 @@
 package Task_1_3_2;
 
+import java.math.BigInteger;
+
 public class Conversion {
+    public static void main(String[] args) {
     char simbolA = 'A';
-    char simbolB = (char)((int)simbolA+1);
-}
+    char simbolB;
+        simbolB = (char)((int)simbolA+1);
+        System.out.println(simbolB);
+        public static char charExpression(int A) {
+            return char SymbolB;
+        }
+    }
